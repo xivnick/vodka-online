db98fe32613826cd0d4376f7d1205fc8192063e8
김민재 <xivnick@gmail.com>
2021-01-13 21:08:44 +0900

    예비 방 만들기

--- data/data.js
+++ data/data.js
@@ -5,6 +5,7 @@ const RoomList = require('../models/RoomList');
 let roomList = new RoomList();
 
 roomList.addRoom('펭귄파티를 해봅시다!', constants.PENGUIN_PARTY);
+roomList.addRoom('띵컨과더를 해봅시다!', constants.PENGUIN_PARTY);
 
 module.exports = {
     roomList,
